33de9887d368aa97af33379e6571ddd3d2d70a35
Akshar Raaj <akshar@urbanpiper.com>
2024-01-08 15:52:58 +0530

    Added comments

--- enums.py
+++ enums.py
@@ -1,12 +1,14 @@
 from enum import Enum
 
 
+# Represents a question's difficulty level.
 class DifficultyLevel(Enum):
     EASY = 'easy'
     MODERATE = 'moderate'
     HARD = 'hard'
 
 
+# Represents sort order while listing questions
 class OrderDirection(Enum):
     ASC = 'asc'
     DESC = 'desc'
